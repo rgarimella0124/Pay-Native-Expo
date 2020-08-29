650f987be74e3bef679018a525fb7b8e8350ced5
Ram Simran G <ram@neokred.tech>
2020-08-29 11:48:07 +0530

    first commit

--- App.js
+++ App.js
@@ -1,16 +1,9 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import "react-native-gesture-handler";
+import React from "react";
 
-export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
-  );
+import App from "./src";
+
+export default function Main() {
+  return <App />;
 }
+;
